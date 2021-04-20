042107bad8d69442897a6ea652ec753244f1506c
Christian Severin <severinchristian25@gmail.com>
2021-04-20 08:33:33 -0400

    Initial Commit

---
+++ Christian_AllAboutMeE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Christian_AllAboutMeE
+//
+//  Created by Christian Severin on 4/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
